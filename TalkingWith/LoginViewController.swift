5a7675cfeb2c04741db14a2e04375c3f2aff11f1
Yeonhee Jeong <anny7867@naver.com>
2021-06-28 20:33:58 +0900

    feat: activate login button and user can login

--- TalkingWith/LoginViewController.swift
+++ TalkingWith/LoginViewController.swift
@@ -13,12 +13,16 @@ class LoginViewController: UIViewController {
     //https://github.com/raulriera/TextFieldEffects 사용
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var SignUpButton: UIButton!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     
     let remoteConfig = RemoteConfig.remoteConfig()
     var color: String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        try! Auth.auth().signOut()
 
         let statusBar = UIView()
         self.view.addSubview(statusBar)
@@ -34,13 +38,32 @@ class LoginViewController: UIViewController {
         loginButton.backgroundColor = UIColor(hex: color)
         SignUpButton.backgroundColor = UIColor(hex: color)
         
+        loginButton.addTarget(self, action: #selector(loginEvent), for: .touchUpInside)
         SignUpButton.addTarget(self, action: #selector(presentSignUp), for: .touchUpInside)
+        
+        Auth.auth().addStateDidChangeListener { (auth, user) in
+            if user != nil {
+                let view = self.storyboard?.instantiateViewController(identifier: "MainViewController") as! MainViewController
+                self.present(view, animated: true, completion: nil)
+            }
+        }
     }
     
     @objc func presentSignUp() {
         let view = self.storyboard?.instantiateViewController(identifier: "SignUpViewController") as! SignUpViewController
         
         self.present(view, animated: true, completion: nil)
+    }
+    
+    @objc func loginEvent() {
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+            if error != nil {
+                let alert = UIAlertController(title: "Error", message: error.debugDescription, preferredStyle: .alert)
+                
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
     }
     
 
